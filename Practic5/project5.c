4aa8685d03934af013dd8b78c4a62f5b52808998
Saavteen <124266750+Saavteen@users.noreply.github.com>
2023-07-06 17:14:35 +0300

    project5.c

--- Practic5/project5.c
+++ Practic5/project5.c
@@ -6,14 +6,14 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     int n, i;
-    int dp[10001]; // Масив для зберігання послідовностей
-    printf("Введіть довжину послідовності n: ");
+    int dp[10001]; // Array for storing sequences
+    printf("Enter the length of the sequence n: ");
     scanf("%d", &n);
-    dp[1] = 2; // Ініціалізація першого елементу масиву
+    dp[1] = 2; // Initializing the first element of an array
     dp[2] = 3;
     for (i = 3; i <= n; i++) {
         dp[i] = (dp[i - 1] + dp[i - 2]) % 12345;
     }
-    printf("Кількість шуканих послідовностей довжиною %d: %d\n", n, dp[n]);
+    printf("The number of searched sequences of length %d: %d\n", n, dp[n]);
     return 0;
 }
